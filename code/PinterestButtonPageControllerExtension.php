2f99f11f4bf0df0ab00c418a279658d9fd9db467
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-07-23 11:50:44 +0200

    javascript for opeining the pinterest share popup

--- code/PinterestButtonPageControllerExtension.php
+++ code/PinterestButtonPageControllerExtension.php
@@ -18,7 +18,29 @@ class PinterestButtonPageControllerExtension extends Extension {
 				var media = this.getAttribute("data-media");
 				var url = this.getAttribute("data-url");
 				var description = this.getAttribute("data-description");
+				var windowURL = "http://pinterest.com/pin/create/button/" +
+												"?url="+ escape(url) +
+												"&media="+ escape(media) +
+												"&description="+ escape(description);
+
+				PopupCenter(windowURL, "Pin It", 750, 550);
 				console.log('pinIt!',media,url,description);
+				return false;
+			}
+
+			function PopupCenter(url, title, w, h) {
+				var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+				var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+				var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+				var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+				var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+				var top = ((height / 2) - (h / 2)) + dualScreenTop;
+				var newWindow = window.open(url, title, 'toolbar=no, location=no, resizable=no, scrollbars=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+				if (window.focus) {
+						newWindow.focus();
+				}
 			}
 
 			var pinButtons = document.getElementsByClassName("pinIt");
